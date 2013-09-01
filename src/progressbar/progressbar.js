d156e1371132fd1b5d95aa0ad887470c78001e6f
Michael Derfler <derfler.michael@gmail.com>
2013-09-01 18:00:05 +0200

    change the template to work with bs3 and fix the tests, use a statically defined bar type (defined as progress-bar* class in  markup) if no stacked types are availabe and auto type is disabled
    fix(progressbar): fix static progress-bar-* classes
    
    fix(progressbar): use static type if no stacked types or auto type
    
    fix(progressbar): undo modal test changes that slipped in

--- src/progressbar/progressbar.js
+++ src/progressbar/progressbar.js
@@ -12,12 +12,12 @@ angular.module('ui.bootstrap.progressbar', ['ui.bootstrap.transition'])
     var animate = angular.isDefined($attrs.animate) ? $scope.$eval($attrs.animate) : progressConfig.animate;
     var autoType = angular.isDefined($attrs.autoType) ? $scope.$eval($attrs.autoType) : progressConfig.autoType;
     var stackedTypes = angular.isDefined($attrs.stackedTypes) ? $scope.$eval('[' + $attrs.stackedTypes + ']') : progressConfig.stackedTypes;
-
+    var staticType = null;
     // Create bar object
     this.makeBar = function(newBar, oldBar, index) {
         var newValue = (angular.isObject(newBar)) ? newBar.value : (newBar || 0);
         var oldValue =  (angular.isObject(oldBar)) ? oldBar.value : (oldBar || 0);
-        var type = (angular.isObject(newBar) && angular.isDefined(newBar.type)) ? newBar.type : (autoType) ? getStackedType(index || 0) : null;
+        var type = (angular.isObject(newBar) && angular.isDefined(newBar.type)) ? newBar.type : (autoType) ? getStackedType(index || 0) : (this.staticType) ? this.staticType : null;
 
         return {
             from: oldValue,
@@ -30,6 +30,10 @@ angular.module('ui.bootstrap.progressbar', ['ui.bootstrap.transition'])
     function getStackedType(index) {
         return stackedTypes[index];
     }
+    
+    this.setStaticType = function(type) {
+        this.staticType = type;
+    };
 
     this.addBar = function(bar) {
         $scope.bars.push(bar);
@@ -55,6 +59,15 @@ angular.module('ui.bootstrap.progressbar', ['ui.bootstrap.transition'])
         },
         templateUrl: 'template/progressbar/progress.html',
         link: function(scope, element, attrs, controller) {
+            
+            var arCls = element.attr('class').split(' ');
+            for(var i=0, n=arCls.length; i < n; i++) {
+                if(arCls[i].indexOf("progress-bar-") != -1) {
+                    element.removeClass(arCls[i]);
+                    controller.setStaticType(arCls[i].replace("progress-bar-",""));
+               }
+            }
+            
             scope.$watch('value', function(newValue, oldValue) {
                 controller.clearBars();
 
